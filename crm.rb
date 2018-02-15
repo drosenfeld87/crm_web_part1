757aafffc758a3cd39fc0fff8b1d165cf0ec84e7
debbie <drosenfeld87@gmail.com>
2018-02-15 15:04:17 -0500

    start css styling

--- crm.rb
+++ crm.rb
@@ -2,14 +2,16 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/' do
-  erb :index
-end
-
-
-get '/contacts' do
   @contacts = Contact.all
   erb :contacts
 end
+
+
+# get '/contacts' do
+#   @contacts = Contact.all
+#
+#   erb :contacts
+# end
 
 get '/aboutme' do
     @skills = ['graphic design', 'css', 'html']
